ba82d263d18e4c4bc702bdf926830fad455398f0
tomer kizel <tomerk@jfrog.com>
2022-08-16 09:54:02 +0300

    os.File in PersistentWriteR

--- pwriter.go
+++ pwriter.go
@@ -1,25 +1,41 @@
 package pcollection
 
-import "sync"
+import (
+	"os"
+	"sync"
+)
 
 type PersistentWriter struct {
 	Empty       bool         `json:"empty"`
-	FilePath    string       `json:"filePath"`
+	OutputFile  *os.File     `json:"outputFile"`
 	DataChan    chan any     `json:"dataChan"`
 	ErrorsQueue *ErrorsQueue `json:"errorsQueue"`
 	Once        *sync.Once   `json:"once"`
+	JsonKey     string       `json:"jsonKey"`
 }
 
-func NewPersistentWriter(filepath string, maxbuffersize int) (*PersistentWriter, error) {
+func NewPersistentWriter(jsonkey string, outputfile *os.File, maxbuffersize int) (*PersistentWriter, error) {
 	self := PersistentWriter{}
-	self.Empty = filepath == ""
-	self.FilePath = filepath
+	self.Empty = outputfile == nil
+	self.OutputFile = outputfile
 	self.DataChan = make(chan any, maxbuffersize)
 	self.Once = new(sync.Once)
 	self.ErrorsQueue = NewErrorsQueue(maxbuffersize)
+	self.JsonKey = jsonkey
 	return &self, nil
 }
 
 func (pw *PersistentWriter) IsEmpty() bool {
 	return pw.Empty
+}
+
+func (pw *PersistentWriter) GetFilePath() string {
+	if pw.OutputFile != nil {
+		return pw.OutputFile.Name()
+	}
+	return ""
+}
+
+func (pw *PersistentWriter) GetJsonKey() string {
+	return pw.JsonKey
 }
