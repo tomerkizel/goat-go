cd273bc1e31d2294fa46c5bbfa0315e50a2a64a6
tomer kizel <tomerk@jfrog.com>
2022-08-25 14:20:21 +0300

    edited pmap tests

--- pmap_test.go
+++ pmap_test.go
@@ -6,16 +6,40 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCorrectAssignment(t *testing.T) {
+func TestAssignment(t *testing.T) {
 	self := EmptyPMap(1, "hi")
 	new, e := self.Add(2, "Da")
 	assert.NoError(t, e)
 	assert.NotEqual(t, new.mapValue, self.mapValue)
+	fail, e := self.Add("yo", 1)
+	assert.Error(t, e)
+	assert.Nil(t, fail)
 }
 
-func TestWrongAssignment(t *testing.T) {
-	self := EmptyPMap(1, "hi")
-	new, e := self.Add("yo", 1)
+type TestStruct struct {
+	IntVar  int            `json:"intVar"`
+	BoolVar bool           `json:"boolVar"`
+	MapVar  map[string]any `json:"mapVar"`
+}
+
+func TestStructAssignment(t *testing.T) {
+	self := EmptyPMap("", TestStruct{})
+	new, e := self.Add("test", TestStruct{1, true, make(map[string]any, 5)})
+	assert.NoError(t, e)
+	assert.NotEqual(t, self.mapValue, new.mapValue)
+	fail, e := new.Add("fail", 1)
 	assert.Error(t, e)
-	assert.Nil(t, new)
+	assert.Nil(t, fail)
+}
+
+func TestReAssignment(t *testing.T) {
+	self := EmptyPMap(1, "")
+	new, e := self.Add(1, "aaa")
+	assert.NoError(t, e)
+	assert.NotEqual(t, self.mapValue, new.mapValue)
+	assert.Equal(t, new.mapValue[1], "aaa")
+	newer, e := new.Add(1, "bbb")
+	assert.NoError(t, e)
+	assert.NotEqual(t, new.mapValue, newer.mapValue)
+	assert.Equal(t, newer.mapValue[1], "bbb")
 }
