e6762d22211fd210840e04b3ec9663911acdb690
tomer kizel <tomerk@jfrog.com>
2022-08-28 12:19:21 +0300

    adjusted tests

--- pmap_test.go
+++ pmap_test.go
@@ -8,12 +8,14 @@ import (
 
 func TestPMapAssignment(t *testing.T) {
 	self := EmptyPMap(1, "hi")
-	new, e := self.AddOne(2, "Da")
+	new, e := self.SetOne(2, "Da")
 	assert.NoError(t, e)
 	assert.NotEqual(t, new.mapValue, self.mapValue)
-	fail, e := self.AddOne("yo", 1)
+	fail, e := self.SetOne("yo", 1)
 	assert.Error(t, e)
 	assert.Nil(t, fail)
+	keys := new.Keys()
+	assert.Equal(t, keys, []any{2})
 }
 
 type TestStruct struct {
@@ -24,44 +26,56 @@ type TestStruct struct {
 
 func TestPMapStructAssignment(t *testing.T) {
 	self := EmptyPMap("", TestStruct{})
-	new, e := self.AddOne("test", TestStruct{1, true, make(map[string]any, 5)})
+	new, e := self.SetOne("test", TestStruct{1, true, make(map[string]any, 5)})
 	assert.NoError(t, e)
 	assert.NotEqual(t, self.mapValue, new.mapValue)
-	fail, e := new.AddOne("fail", 1)
+	fail, e := new.SetOne("fail", 1)
 	assert.Error(t, e)
 	assert.Nil(t, fail)
 }
 
 func TestPMapReAssignment(t *testing.T) {
 	self := EmptyPMap(1, "")
-	new, e := self.AddOne(1, "aaa")
+	new, e := self.SetOne(1, "aaa")
 	assert.NoError(t, e)
 	assert.NotEqual(t, self.mapValue, new.mapValue)
 	assert.Equal(t, new.mapValue[1], "aaa")
-	newer, e := new.AddOne(1, "bbb")
+	newer, e := new.SetOne(1, "bbb")
 	assert.NoError(t, e)
 	assert.NotEqual(t, new.mapValue, newer.mapValue)
 	assert.Equal(t, newer.mapValue[1], "bbb")
 }
 
-func TestPMapReadDelete(t *testing.T) {
+func TestPMapMethods(t *testing.T) {
 	arr := [4]string{"a", "b", "c", "d"}
 	mapval := map[any]any{1: "a", 2: "b", 3: "c", 4: "d"}
 	self := EmptyPMap(1, "")
-	self, e := self.AddBatch(mapval)
+	self, e := self.SetMany(mapval)
 	assert.NoError(t, e)
 	for i := 1; i <= 4; i++ {
-		val, e := self.Read(i)
+		val, e := self.Get(i)
 		assert.NoError(t, e)
 		assert.Equal(t, val, arr[i-1])
 	}
-	val, e := self.Read("1")
+	val, e := self.Get("1")
 	assert.Error(t, e)
 	assert.Nil(t, val)
-	fail, e := self.Delete("ar")
+	fail, item, e := self.Delete("ar")
 	assert.Error(t, e)
 	assert.Nil(t, fail)
-	suc, e := self.Delete(1)
+	assert.Nil(t, item)
+	suc, item, e := self.Delete(1)
 	assert.NoError(t, e)
 	assert.Nil(t, suc.mapValue[1])
+	assert.Equal(t, item, "a")
+}
+
+func TestPMapNil(t *testing.T) {
+	self := EmptyPMap(nil, nil)
+	new, e := self.SetOne(1, "hi")
+	assert.NoError(t, e)
+	assert.Equal(t, new.mapValue, map[any]any{1: "hi"})
+	new, e = new.SetOne("hello", 2)
+	assert.NoError(t, e)
+	assert.Equal(t, new.mapValue, map[any]any{1: "hi", "hello": 2})
 }
