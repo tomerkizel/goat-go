e94916006d25332fe43e66e91c4a0786d9d28d5e
tomer kizel <tomerk@jfrog.com>
2022-08-28 12:30:57 +0300

    added nil values to CheckType

--- utils/utils.go
+++ utils/utils.go
@@ -7,6 +7,9 @@ import (
 
 //CheckType returns an error if a and b of type any are not of the same real type. Returns nil otherwise
 func CheckType(a, b any) error {
+	if a == nil || b == nil {
+		return nil
+	}
 	if reflect.TypeOf(a).Kind() != reflect.TypeOf(b).Kind() {
 		return fmt.Errorf("vairable of type %v should be of type %v", reflect.TypeOf(a).Kind(), reflect.TypeOf(b).Kind())
 	}
